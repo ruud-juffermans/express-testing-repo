6c2a9345e7da468cac96fb87c58ba3e148970b9c
Ruud Juffermans <ruudjuffermans@pm.me>
2026-01-01 12:28:58 +0100

    rm cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// middleware
-app.use(cors());
 app.use(express.json());
 
 // health check
